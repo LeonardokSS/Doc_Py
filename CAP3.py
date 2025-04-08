a894671dcea51bfab904246e52e02651d1080908
LeonardoKaprio <leonardoken02@gmail.com>
2025-04-08 09:47:45 -0300

    Add files via upload

---
+++ CAP3.py
@@ -0,0 +1,10 @@
+Comentários = #comentário
+
+numeros = "float ou int"
+
+string = "isso e uma string"
+
+Texto = "/n" abre uma nova linha, "texto1/nTexto2"
++ = soma duas trings "sss"+"ooo" = "ssso'oo"
+Strings perto uma da outra se fecham automaticamente 'ss'  'ss'= 'ssss
+'
