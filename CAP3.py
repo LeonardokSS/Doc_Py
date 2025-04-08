4cb5ebdc68f50a8fa342456ba26c9a378176ce44
LeonardoKaprio <leonardoken02@gmail.com>
2025-04-08 11:13:45 -0300

    Update CAP3.py

--- CAP3.py
+++ CAP3.py
@@ -5,6 +5,22 @@ numeros = "float ou int"
 string = "isso e uma string"
 
 Texto = "/n" abre uma nova linha, "texto1/nTexto2"
-+ = soma duas trings "sss"+"ooo" = "ssso'oo"
+
++ = soma duas trings "sss"+"ooo" = "sssooo"
+
 Strings perto uma da outra se fecham automaticamente 'ss'  'ss'= 'ssss
-'
+
+text = ('Quebra a linha para texto muito '
+        'graaaaanddes.')
+
+ +---+---+---+---+---+---+
+ | P | y | t | h | o | n |
+ +---+---+---+---+---+---+
+ 0   1   2   3   4   5   6
+
+ print com essas posições para poder selecionar a letra   -- a letra deve estar entre os numeros do exemplo
+ ex: print[0:1]
+ >>> P
+ 
+
+
