a74aef3953511a437ac5851813684af13d43d145
LeonardoKaprio <leonardoken02@gmail.com>
2025-05-12 13:38:38 -0300

    Add files via upload

---
+++ Basic Syntax/Syntaxe.py
@@ -0,0 +1,29 @@
+#A sintaxe Python define um conjunto de regras que são utilizadas para criar um programa Python. A sintaxe da linguagem de programação Python tem muitas semelhanças com as linguagens de programação Perl, C e Java. No entanto, existem algumas diferenças definitivas entre as linguagens.
+
+# Variaveis são como uma caixa que armazena informações de todos os tipos e pode manipular os mesmos
+fruta = 'Banana'
+n = 25
+pi = 3.141592653589931
+
+#Tipos de dados 
+#Inteiros para numeros como: 1,20,399
+#Pontos 
+
+#Linhas lógicas
+#A instrução completa que pode ocupar várias linhas físicas, mas que o Python entende como uma só.
+
+#Linhas físicas
+#Literalmente uma linha fisica composta por caracteres e encerrada por enter ou \
+
+soma = 1 + 2 + 3 \ 
+       3 + 4 + 5
+# 2 linhas fisicas que formam uma linha lógica
+
+# Junção de linha explícita
+#Duas ou mais linhas físicas podem ser juntadas em linhas lógicas usando o caractere contrabarra (\) da seguinte forma: quando uma linha física termina com uma contrabarra que não é parte da uma literal string ou comentário, ela é juntada com a linha seguinte formando uma única linha lógica, removendo a contrabarra e o caractere de fim de linha seguinte.
+
+# Junção de linha implícita 
+#Expressões edntre ( ), [ ],{ } podem ser quebradas em linhas físicas sem a necessidae de utilizar \
+
+#Linhas em branco
+#Uma linha lógica que contém apenas espaços, tabulações, quebras de página e possivelmente um comentário é ignorada (ou seja, nenhum token NEWLINE é gerado). Durante a entrada interativa de instruções, o tratamento de uma linha em branco pode diferir dependendo da implementação do interpretador. No interpretador interativo padrão, uma linha lógica totalmente em branco (ou seja, uma que não contenha nem mesmo espaço em branco ou um comentário) encerra uma instrução de várias linhas.
