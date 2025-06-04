c570104715330ead112a82ea9230a704c58b6af8
LeonardoKaprio <leonardoken02@gmail.com>
2025-06-04 19:04:36 -0300

    Atualização de melhoria

--- Basic Syntax/Classe.py
+++ Basic Syntax/Classe.py
@@ -1,17 +1,20 @@
 #Classe e um molde que cria objetos e define quais características e funções eles iram ter
+
+#No self, estou basicamente criando um objeto
+#No init, ele diz que a partir dele, ele vai começar a receber os valores para o objeto e depois os valores do objeto são iguais as valores das variáveis
 class Carro:
     def __init__(self, marca, cor):
         self.marca = marca
-        self.cor = cor             #No self, estou basicamente dizendo que o input é igual as variáveis da class
+        self.cor = cor             
     def Buzinar(self):
         print("BIBIBI!!!")
     
-    #aqui defino características para um carro que criei
+#Aqui defino características para um carro que criei
 carro_pai = Carro("Ford","Vermelho")
 meu_carro = Carro("Chevrolet","Preto")
 
-print(carro_pai.cor) #aqui eu faço a requisão da cor do carro do pai
-print(carro_pai.marca) #aqui da marcar do carro do pai
+print(carro_pai.cor) #aqui eu faço a requisão do objeto que eu criei e tem o valor da cor
+print(carro_pai.marca) #mesma coisa porém para marcas do carro
 
 
 #No exemplo dos peixe temos agora um Method que retorna a resposta formatada pela função que pega o self 
