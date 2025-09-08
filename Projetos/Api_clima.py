30259c89151728472e7d0c3ada050dd943b27214
LeonardoKaprio <leonardoken02@gmail.com>
2025-09-08 18:45:33 -0300

    Chance de chuva

--- Projetos/Api_clima.py
+++ Projetos/Api_clima.py
@@ -4,23 +4,36 @@ import json
 chave = "7e7f140038d14ff44e53ba8feab0b659"
 
 #Cidade para verificar
-cidade = input("Digite o nome da cidade que você deseja consultar: ")
+Cidade = input("Digite o nome da Cidade que você deseja consultar: ")
 
 #Url da requisição
-url = f"http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave}&units=metric&lang=pt_br"
+Clima = f"http://api.openweathermap.org/data/2.5/weather?q={Cidade}&appid={chave}&units=metric&lang=pt_br"
+
 
 
 #Variavel para armazenar o valor dos dados
-requerirClima = rq.get(url)
+requerirClima = rq.get(Clima)
 
 #Transoformar os dados em JSON
 Dados = requerirClima.json()
 
-
-
 temp = Dados["main"]["temp"]
-desc = Dados["weather"][0]["description"]
+
 umidade = Dados["main"]["humidity"]
 
+#Url para a Chuva
+Chuva = f"https://api.openweathermap.org/data/2.5/forecast?q={Cidade}&units=metric&appid={chave}&lang=pt_br"
 
-print(f"A temperatura da cidade {cidade} é {temp} C\nO tempo está:{desc}\nA umidade é {umidade}%")
+
+requirirChuva = rq.get(Chuva)
+
+DadosChuva = requirirChuva.json()
+
+
+Condicao = DadosChuva["list"][0]["weather"][0]["description"]
+Chance = DadosChuva["list"][0]["pop"]
+
+print(f"A situação atual é de: {Condicao}\nA chance de chuva é {Chance}%")
+
+
+print(f"A temperatura da Cidade de {Cidade} é {temp} C\nA umidade é {umidade}%")
