c285a0e56b0b4ba30e8731dc0132ffe9a3289fc3
LeonardokSS <leonardoken02@gmail.com>
2026-01-27 11:40:59 -0300

    ProjetoSenha Melhorado

--- Projetos/Gerador_senhas.py
+++ Projetos/Gerador_senhas.py
@@ -1,11 +1,12 @@
 import string
 import random
+from colorama import Fore,Style
 
 
 NUM = int(input("Digite o numero de caracteres entre 8 a 32: "))
 
 
- 
+
 #Letras recebe todos as letras do alfabeto
 letras = string.ascii_letters
 
@@ -32,46 +33,58 @@ for letra in letras:
 Lista_string = []
 Final = ""
 
+
     
 
 def GerarSenha():
-        Tipo_senha = int(input("***********************************      Gerador de Senhas         *********************************** \nEscolha o nível da senha:\n1 Apenas letras / 2 Letras e números / 3 Letras, números e símbolos \n Digite o número correspondente ao nível desejado:")) 
+        Tipo_senha = int(input("""
+***********************************
+        Gerador de Senhas
+***********************************
 
+Escolha o nível da senha:
+1 - Apenas letras
+2 - Letras e números
+3 - Letras, números e símbolos
 
-
+Digite o número correspondente ao nível desejado: 
+"""))
         
         if Tipo_senha == 1:
-                for n in range(NUM):
+                for i in range(NUM):
                     Senha = random.choices(letras_lista)
                     #Transforma para string
                     SenhaSTR = "".join(Senha)
                     #Adiciona lista
                     Lista_string.append(SenhaSTR)
+                    
                     final = "".join(Lista_string)
-                    print("A sua nova senha é:  {}".format(final))
-
+                    
+                print(f"A sua nova senha é: {Fore.YELLOW}{final}{Style.RESET_ALL}")
         elif Tipo_senha == 2:
+                
                 for n in range(NUM):
-                    Senha = random.choices(letras_lista,numeros_lista)
-                    #Transforma para string
-                    SenhaSTR = "".join(Senha)
+                    ListaEscolhida = random.choice([letras_lista,numeros_lista])
+                    Escolhido = random.choice(ListaEscolhida)
                     #Adiciona lista
-                    Lista_string.append(SenhaSTR)
+                    Lista_string.append(Escolhido)
+                    
                     final = "".join(Lista_string)
-                    print("A sua nova senha é:  {}".format(final))
+                print(f"A sua nova senha é: {Fore.YELLOW}{final}{Style.RESET_ALL}")
 
         elif Tipo_senha == 3:
                 for n in range(NUM):
-                    Senha = random.choices(letras_lista,numeros_lista,pontuacao_lista)
-                    #Transforma para string
-                    SenhaSTR = "".join(Senha)
+                    ListaEscolhida = random.choice([letras_lista,numeros_lista,pontuacao_lista])
+                    Escolhido = random.choice(ListaEscolhida)
+                  
+                    
                     #Adiciona lista
-                    Lista_string.append(SenhaSTR)
+                    Lista_string.append(Escolhido)
                     final = "".join(Lista_string)
-                    print("A sua nova senha é:  {}".format(final))
+                print(f"A sua nova senha é: {Fore.YELLOW}{final}{Style.RESET_ALL}")
 
         else:
-                print("Erro nas opções")
+                print(f"{Fore.RED}{"Digite uma opção valida!"}{Style.RESET_ALL}")
                 
             
 GerarSenha()
